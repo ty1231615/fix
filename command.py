0729681b933e1c8f488a949fde06867637ef3ed5
ty <56197216+ty1231615@users.noreply.github.com>
2022-06-30 05:13:06 +0000

    comit

---
+++ command.py
@@ -0,0 +1,14 @@
+from this import d
+import discord
+
+class Handler:
+    def __init__(self) -> None:
+        pass
+    
+
+class Command:
+    def __init__(self) -> None:
+        pass
+    @classmethod
+    def recv(cls):
+        pass
--- core.py
+++ core.py
@@ -1,1 +1,19 @@
+import discord
+import asyncio
+import setting
 
+
+client = discord.Client()
+TOKEN = setting.TOKEN
+
+@client.event
+async def on_ready():
+    print("logined")
+
+@client.event
+async def on_message(message):
+    if message.guild.id == 824527491614900224:
+        await message.channel.send(message.content)
+
+
+client.run(TOKEN)
